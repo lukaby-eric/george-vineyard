5a26b221f2c6453bb4d78496195b87d5aad56cce
Lukas lubingo <78145529+lukaby-eric@users.noreply.github.com>
2023-07-29 11:27:32 -0500

    Update script.js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var form = document.getElementById("contact-form");
+
+
+function openForm() {
+    form.style.transform = 'scale(1,1)';
+}
+function closeForm() {
+    form.style.transform = 'scale(0,0)';
+}
